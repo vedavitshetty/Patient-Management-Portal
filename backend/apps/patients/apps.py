223a1dd28b3570c9c6a698739ae3b6081c929e52
Vedavit Shetty <vedavit@berkeley.edu>
2024-02-02 00:58:02 -0800

    setup patients model in Django Admin

--- backend/apps/patients/apps.py
+++ backend/apps/patients/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PatientAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patient_app'
+    name = 'apps.patients'
