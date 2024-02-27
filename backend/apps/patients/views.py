61261c2e0fb13cbf0bd91cf28ca5d0958ac9e3b1
Vedavit Shetty <vedavit@berkeley.edu>
2024-02-27 02:24:16 -0800

    backend lint

--- backend/apps/patients/views.py
+++ backend/apps/patients/views.py
@@ -11,19 +11,23 @@ class PatientRUD(generics.RetrieveUpdateDestroyAPIView):
 
 class PatientListOrCreate(generics.ListCreateAPIView):
     serializer_class = PatientListOrCreateSerializer
-    
+
     def get_queryset(self):
-        churned_patients = Patient.objects.filter(status='CHURNED').order_by("first_name")
-        non_churned_patients = Patient.objects.exclude(status='CHURNED').order_by("first_name")
-        return {'churned': churned_patients, 'non_churned': non_churned_patients}
+        churned_patients = Patient.objects.filter(status="CHURNED").order_by(
+            "first_name"
+        )
+        non_churned_patients = Patient.objects.exclude(status="CHURNED").order_by(
+            "first_name"
+        )
+        return {"churned": churned_patients, "non_churned": non_churned_patients}
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        churned_serializer = self.get_serializer(queryset['churned'], many=True)
-        non_churned_serializer = self.get_serializer(queryset['non_churned'], many=True)
+        churned_serializer = self.get_serializer(queryset["churned"], many=True)
+        non_churned_serializer = self.get_serializer(queryset["non_churned"], many=True)
 
         response_data = {
-            'churned': churned_serializer.data,
-            'non_churned': non_churned_serializer.data
+            "churned": churned_serializer.data,
+            "non_churned": non_churned_serializer.data,
         }
         return Response(response_data)
